f38b77a3a7e7175fee4a158695b88b926c55ce6a
okurmanPython <okurman.kanatkasymbekovich007@gmail.com>
2025-03-13 20:08:36 +0600

    set жонундо тушунук

---
+++ home_works.py
@@ -0,0 +1,15 @@
+# set - множество - коптуктор: ичинен дубликаттарды алып уникальный созду эле чыгарып берет
+nums = {1,1,1,2,2,2,3,3,3}
+names = {
+    "kanat",
+    "grad",
+    "kanat",
+    "shergazy",
+    "ernis",
+    "begimay",
+    "aiana",
+}
+name = input("enter the name: ")
+print(names)
+names.add(name)
+print(names)

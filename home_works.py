18b4b9966d9a51d75e5a5d45af5390429252d303
okurmanPython <okurman.kanatkasymbekovich007@gmail.com>
2025-03-14 15:57:46 +0600

    Бул 13.03 уй тапшырмасы

---
+++ home_works.py
@@ -0,0 +1,29 @@
+# task 1
+def greet():
+    print("Привет, Пользователь!")
+greet()
+greet()
+
+# task 2
+def weather():
+    print("Сегодня 15^C, солнечно")
+weather()
+
+# task 3
+def favorite_food():
+    print("Мое любимое блюдо - пицца.")
+favorite_food()
+
+# task 4
+def sum_numbers():
+    print(f"7 + 3 = 10")
+sum_numbers()
+
+# task 5
+def repeat_phrase():
+   for i in range(3):
+    print(f"Учиться - это круто!")
+repeat_phrase()
+
+
+

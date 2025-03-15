58c7a3568c8099449b6b9bfe6f65f0d3cb13a574
okurmanPython <okurman.kanatkasymbekovich007@gmail.com>
2025-03-15 19:37:04 +0600

    learn return

---
+++ return.py
@@ -0,0 +1,18 @@
+# return - кайтаруу
+
+# def add(a, b):
+#     print(a + b)
+#     # return a + b
+# res = add(1, 2)
+# print(res)
+
+# def weather(celsius):
+#     if celsius < 0:
+#         return "Аба ырайы суук"
+#     elif 0 < celsius < 20:
+#         return "Аба ырайы жылуу"
+#     else:
+#         return "Аба ырайы ысык"
+# print(weather(70))
+
+

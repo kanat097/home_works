4cc4092eb9f25c86fae840759a0b6841c1071f2f
okurmanPython <okurman.kanatkasymbekovich007@gmail.com>
2025-03-20 19:40:32 +0600

    20.03 тема

---
+++ home_works.py
@@ -0,0 +1,7 @@
+import random  #Импорт всего файла/модуля
+a = random.randint(0, 10)
+from random import randint # Импорт конкретных объектов из модуля
+x = randint(0, 10)
+import datetime as dt # Импорт всего модуля с псевдонимом
+now = dt.datetime.now()
+from random import * # Импорт всех объектов из модуля
